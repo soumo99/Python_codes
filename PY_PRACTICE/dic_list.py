952369409ee7954b230d797b31f383fb60cb1b33
Soumobrata Manna <82120717+soumo99@users.noreply.github.com>
2022-03-29 23:03:44 +0530

    Add files via upload

---
+++ PY_PRACTICE/dic_list.py
@@ -0,0 +1,12 @@
+#Accessing dicitionary values inside a list
+
+class_list = [
+    {"Name":"SoumobrataManna" , "class":"A", "subject":"Maths"},
+    {"Name":"Gowtham" , "class":"B" ,"subject":"Computer"},
+    {"Name":"Subrata" , "class":"C" ,"subject":"Chemistry"},
+    {"Name":"Anindita" , "class":"D" ,"subject":"English"},
+    {"Name":"Sayandeep" , "class":"E", "subject":"Biology"},
+    {"Name":"Gaurav" , "class":"F" ,"subject":"Geography"},
+
+]
+print(class_list)
